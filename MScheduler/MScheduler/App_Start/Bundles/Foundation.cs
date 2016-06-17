8fe5b0f19c4af31c3045be7658ba9d1367766d11
Chaudhry Mohsin Ali <mohsinali1017@gmail.com>
2016-06-17 13:09:03 +0200

    done with html parsing of teams just encoding of characters is missing

---
+++ MScheduler/MScheduler/App_Start/Bundles/Foundation.cs
@@ -0,0 +1,26 @@
+using System.Web.Optimization;
+
+namespace MScheduler
+{
+    public static class Foundation
+    {
+        public static Bundle Styles()
+        {
+            return new StyleBundle("~/Content/foundation/css").Include(
+                       "~/Content/foundation/foundation.css",
+                       "~/Content/foundation/foundation.mvc.css",
+                       "~/Content/Site.scss", // todo: take it from here
+                       "~/Content/foundation/app.css");
+        }
+
+        public static Bundle Scripts()
+        {
+            return new ScriptBundle("~/bundles/foundation").Include(
+                      "~/Scripts/foundation/fastclick.js",
+                      "~/Scripts/jquery.cookie.js",
+                      "~/Scripts/foundation/foundation.js",
+                      "~/Scripts/foundation/foundation.*",
+                      "~/Scripts/foundation/app.js");
+        }
+    }
+}
--- MScheduler/MScheduler/Controllers/HomeController.cs
+++ MScheduler/MScheduler/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,7 +12,49 @@ namespace MScheduler.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            List<string> Eliteserie = new List<string>();
+            List<string> FirstDivision = new List<string>();
+            List<string> SecondDivision = new List<string>();
+            List<string> ThirdDivision = new List<string>();
+            List<string> FourthDivision = new List<string>();
+            List<string> FifthDivision = new List<string>();
+
+
+            // Creates an HtmlDocument object from an URL
+            var html = new HtmlDocument();
+
+            Uri u = new Uri("http://cricketforbundet.no/index.php/en/klubber");
+
+            html.LoadHtml(new WebClient().DownloadString(u));
+
+            var root = html.DocumentNode;
+            var p = root.Descendants("table").FirstOrDefault().Descendants("tr").Skip(1);
+
+            foreach (var item in p)
+            {
+                Eliteserie.Add(item.ChildNodes.Where(i => i.Name == "td").FirstOrDefault().InnerText);
+                FirstDivision.Add(item.ChildNodes.Where(i => i.Name == "td").ElementAt(1).InnerText);
+                SecondDivision.Add(item.ChildNodes.Where(i => i.Name == "td").ElementAt(2).InnerText);
+                ThirdDivision.Add(item.ChildNodes.Where(i => i.Name == "td").ElementAt(3).InnerText);
+            }
+
+            var p1 = root.Descendants("table").ElementAt(1).Descendants("tr").Skip(2);
+
+            foreach (var item in p1)
+            {
+                FourthDivision.Add(item.ChildNodes.Where(i => i.Name == "td").FirstOrDefault().InnerText);
+                FifthDivision.Add(item.ChildNodes.Where(i => i.Name == "td").ElementAt(1).InnerText);
+            }
+
+            List<dynamic> teams = new List<dynamic>();
+            teams.Add(Eliteserie);
+            teams.Add(FirstDivision);
+            teams.Add(SecondDivision);
+            teams.Add(ThirdDivision);
+            teams.Add(FourthDivision);
+            teams.Add(FifthDivision);
+
+            ViewBag.teams = teams;
 
             return View();
         }
