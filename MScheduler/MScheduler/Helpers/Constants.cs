a6475aadd152e9be6ab28a89fcbc8c23199e0e39
Chaudhry Mohsin Ali <mohsinali1017@gmail.com>
2016-06-21 13:54:27 +0200

    starting with match making

---
+++ MScheduler/MScheduler/Helpers/Constants.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MScheduler.Helpers
+{
+    public class Constants
+    {
+        public static List<string> grounds = new List<string>{
+            "Ekerbeg1",
+            "Ekerbeg1",
+            "Rommen",
+            "Stubermyra",
+            "Drammen",
+            "Bærum"
+        };
+
+        public static List<DateTime> exceptionDates = new List<DateTime>{
+           new DateTime(2016,7,4)
+        };
+
+        public static DateTime startDate = new DateTime(2016,7,1);
+
+        public static DateTime endDate = new DateTime(2016, 7, 31);
+
+        public static int matchesPerTeamOnWeekend = -1; // if -1 then we don't care about this
+ 
+
+
+
+    }
+}
---
+++ MScheduler/MScheduler/Models/Match.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MScheduler.Models
+{
+    public class Match
+    {
+        public string firstTeam;
+        public string secondTeam;
+        public DateTime date;
+        public DateTime time;
+        public string ground;
+        public string day;
+        public string division;
+    }
+}
